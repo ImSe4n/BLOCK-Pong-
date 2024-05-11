74e85f758135a15a966f3a024b158a45d80bec37
Sean Nie <81334381+ImSe4n@users.noreply.github.com>
2024-05-11 14:06:44 -0400

    define variables, delete other folder

--- sketch.js
+++ sketch.js
@@ -2,17 +2,37 @@
 Courtney Edwards - May 2021
 This file shows an example for a default sprite*/
 
+//define variables
+let val = 2
+var bx = 400;
+var by =  230;
+var cx = 60;
+var cy = 100;
+let boxSize = 50;
+
+//level variables
+var level = 0;
 var boxSprite;
-
-function setup() {
-  createCanvas(800,400);
-  //this is to create a default sprite. By including 50,50 at the end (optional), it sets the size of the rectangular sprite
-  //by assigning the sprite to a variable, we can change properties of the sprite
-  boxSprite = createSprite(400, 200, 50, 50);
-}
-
-function draw() {
-  background(255,255,255); 
-  //drawSprites() is necessary in order to see the sprites on the screen 
-  drawSprites();
-}
+var mousePosition;
+var spriteBalls;
+var padWidth = 12;
+var ballDiameter = 15;
+var oppenentScore = 0;
+var timeGame = 8;
+var spriteBot;
+var speedBot = 5;
+let frictionBot = 0.2;
+var padLength = 120;
+let stupidSlider = false;
+var yourScore = 0;
+let yesSlider = false;
+let turnOnSlider = false;
+let levelZero = false;
+var fadingBlock;
+let lotsOfBlocks;
+let lotsOfBlocks2;
+let lotsOfBlocks3;
+let buttonLives;
+let buttonReset;
+let overBox8 = false;
+let rightLevel = false;
